09bf04be686f653bfa07d67fe72ff92c0aca6a45
asmodaip <asmodai@epdofs16plt0sci1gr81.auto.internal>
2022-04-30 14:56:20 +0300

    add culic files

---
+++ culic_position.py
@@ -0,0 +1,15 @@
+
+import os
+import sys
+from position import update_sheet
+import datetime as dt
+# получаем имя клиента капсом
+client = os.path.splitext (os.path.basename (sys.argv [0]))[0].split('_')[0].upper()
+
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+ID_FOR_NOTIFICATION = os.environ['ID_FOR_NOTIFICATION'].split(',')
+# #берем соответвтующей id в .env
+SPREADSHEET_ID = os.environ[f'{client}_SPREADSHEET_ID']
+
+range_name = dt.datetime.now().strftime('%m.%Y')
+update_sheet(SPREADSHEET_ID,range_name)
